4e3c0336211ea084c591a2b7e48d662e25d6caaa
Pawan Deshpande <idlivada@gmail.com>
2014-07-05 10:51:47 -0400

    Add support for admin password reset.  Fixes #5.

--- campaign/urls.py
+++ campaign/urls.py
@@ -11,4 +11,11 @@ urlpatterns = patterns('',
     url(r'^call/', 'core.views.call', name='call'),
     url(r'^dial-callback/', 'core.views.dial_callback', name='dial-callback'),
     url(r'^s/(?P<slug>[a-zA-Z0-9_.-]+)/', 'core.views.single_campaign', name='single-campaign'),
+
+    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
+       (r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+       (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+       (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+
+
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
